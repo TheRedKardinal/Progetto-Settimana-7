57dca6b1fcdc79219217b4d6e6c055daae713aa0
TheRedKardinal <claudio_giasi@icloud.com>
2026-06-19 16:07:01 +0200

    Render fatto

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,7 +55,7 @@ async function cercaSquadre(query) {
         if (dati.teams === null) {
             return 'Nessuna squadra corrisponde alla ricerca';
         }
-        const squadre = dati.teams.map(item => new Squadra(item.idTeam, item.strTeam, item.strTeamBadge, item.strLeague, item.strCountry));
+        const squadre = dati.teams.map(item => new Squadra(item.idTeam, item.strTeam, item.strBadge, item.strLeague, item.strCountry));
         return squadre;
     } catch (errore) {
         console.error(errore);
@@ -89,7 +89,55 @@ async function caricaDettagli(idTeam) {
 
 
 // === Render ===
+const contenitoreSquadre = document.getElementById('lista-squadre');
 
+function renderSquadre(squadre) {
+    contenitoreSquadre.replaceChildren();
+    if (typeof squadre === 'string') {
+        const p = document.createElement('p');
+        p.textContent = squadre;
+        p.classList.add('text-center');
+        contenitoreSquadre.appendChild(p);
+        return;
+    }
+
+    squadre.forEach(squadra => {
+        const col = document.createElement('div');
+        col.classList.add('col-12', 'col-md-6', 'col-lg-4');
+
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        col.appendChild(card);
+        contenitoreSquadre.appendChild(col);
+
+        const img = document.createElement('img');
+        img.src = squadra.logo;
+        img.classList.add('card-img-top');
+
+        const cardBody = document.createElement('div');
+        cardBody.classList.add('card-body', "d-flex", "flex-column", "align-items-center");
+
+        const titolo = document.createElement('h5');
+        titolo.classList.add('card-title', 'text-center');
+        titolo.textContent = squadra.nome;
+
+        const sottotitolo = document.createElement('p');
+        sottotitolo.classList.add('card-text', 'text-center');
+        sottotitolo.textContent = squadra.lega + '-' + squadra.paese;
+
+        const bottone = document.createElement('button');
+        bottone.classList.add('btn', 'btn-primary', 'w-100');
+        bottone.textContent = 'Vedi dettagli';
+        bottone.addEventListener('click', () => caricaDettagli(squadra.id));
+
+        cardBody.appendChild(titolo);
+        cardBody.appendChild(sottotitolo);
+        cardBody.appendChild(bottone);
+        card.appendChild(img);
+        card.appendChild(cardBody);
+    });
+}
 
 
 // === Eventi ===
@@ -98,5 +146,5 @@ form.addEventListener('submit', async (e) => {
     e.preventDefault();
     const query = document.getElementById('search').value;
     const risultato = await cercaSquadre(query);
-    console.log(risultato);
+    renderSquadre(risultato);
 })
